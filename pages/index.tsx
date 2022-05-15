47a7c9c4a656142354376c46a351e45276735263
Ian Sutherland <ian@iansutherland.ca>
2022-05-15 16:19:33 -0600

    Update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
+    <p>Hello, World!</p>
     <p>
       <Link href="/about">
         <a>About</a>
