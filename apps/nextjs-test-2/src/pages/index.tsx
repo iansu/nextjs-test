dbe18dc724cec75ee673318fc3beecdc20ed47ab
Ian Sutherland <ian@iansutherland.ca>
2022-05-16 14:24:14 -0600

    Update test app 2

--- apps/nextjs-test-2/src/pages/index.tsx
+++ apps/nextjs-test-2/src/pages/index.tsx
@@ -5,6 +5,7 @@ import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello from Test App 2</h1>
+    <p>Adding a message to test app 2</p>
     <Button>Click me</Button>
   </Layout>
 )
